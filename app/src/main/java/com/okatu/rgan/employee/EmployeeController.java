10baaaeca010c127cb5ba2c20c6cc4e807ccce6e
yutianhuihong <loveshady5@163.com>
2020-01-21 16:06:48 +0800

    docker test

--- app/src/main/java/com/okatu/rgan/employee/EmployeeController.java
+++ app/src/main/java/com/okatu/rgan/employee/EmployeeController.java
@@ -1,59 +1,12 @@
 package com.okatu.rgan.employee;
 
-import com.okatu.rgan.employee.EmployeeNotFoundException;
-import com.okatu.rgan.employee.Employee;
-import com.okatu.rgan.employee.EmployeeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
     private static Logger logger = LoggerFactory.getLogger(EmployeeController.class);
-
-    private final EmployeeRepository repository;
-
-    public EmployeeController(EmployeeRepository repository) {
-        this.repository = repository;
-    }
-
-    @GetMapping("/employees")
-    Page<Employee> all(@PageableDefault(size = 1) Pageable pageable){
-        return repository.findAll(pageable);
-    }
-
-    @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee employee){
-        return repository.save(employee);
-    }
-
-    @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id){
-        return repository.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException(id));
-    }
-
-    @PutMapping("/employees/{id}")
-    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-        return repository.findById(id)
-                .map(employee -> {
-                    employee.setName(newEmployee.getName());
-                    employee.setRole(newEmployee.getRole());
-                    return repository.save(employee);
-                })
-                .orElseGet(() -> {
-                    newEmployee.setId(id);
-                    return repository.save(newEmployee);
-                });
-    }
-
-    @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id){
-        repository.deleteById(id);
-    }
 
     @GetMapping("/hello")
     String helloWorld(){
--- app/src/main/java/com/okatu/rgan/employee/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.okatu.rgan.employee;
-
-import com.okatu.rgan.employee.Employee;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
-}
-
-
