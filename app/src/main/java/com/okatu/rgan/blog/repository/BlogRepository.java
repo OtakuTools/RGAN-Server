c67ab14ba463ecb376b976f9a2e0818af83e3267
yutianhuihong <loveshady5@163.com>
2020-06-30 18:49:14 +0800

    add show sql for debug

--- app/src/main/java/com/okatu/rgan/blog/repository/BlogRepository.java
+++ app/src/main/java/com/okatu/rgan/blog/repository/BlogRepository.java
@@ -13,6 +13,13 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Collection;
 import java.util.Optional;
 
+// What choice have you got?
+// result mapping is for native query
+// for the Entity, can use simple JPQL and designate fetch strategy
+// but for projection?
+// or, we need to cache the projection?
+// and we need to use mechanism than can be integrated into current system
+// like l2 cache, stuff like UserSummary need to be cached
 public interface BlogRepository extends JpaRepository<Blog, Long>, CustomizedBlogRepository {
 
     Page<BlogSummaryProjection> findByAuthorInAndStatusOrderByCreatedTimeDesc(Collection<RganUser> author, BlogStatus status, Pageable pageable);
